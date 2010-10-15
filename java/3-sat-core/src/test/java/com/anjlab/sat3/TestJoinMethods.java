e4a1fa5865f223321a08b9d7f97b5912f0d6a3ed
Dmitry Gusev <dmitry.gusev@gmail.com>
2010-10-15 13:53:52 +0400

    More tests for JoinMethods

--- java/3-sat-core/src/test/java/com/anjlab/sat3/TestJoinMethods.java
+++ java/3-sat-core/src/test/java/com/anjlab/sat3/TestJoinMethods.java
@@ -22,7 +22,7 @@ public class TestJoinMethods
     }
 
     @Test
-    public void testJoinBetweenTiers2() throws Throwable
+    public void testJoin2BetweenTiers() throws Throwable
     {
         for (int a1 = 1; a1 <= 3; a1++)
         for (int b1 = 1; b1 <= 3; b1++)
@@ -61,6 +61,77 @@ public class TestJoinMethods
                         throw t;
                     }
                 }
+    }
+
+    @Test
+    public void testJoin2BetweenTiers_BitsOnTheirPlaces() throws Throwable
+    {
+        for (int a1 = -3; a1 <= 3; a1 = a1 == -1 ? 1 : a1+1)
+        for (int b1 = -3; b1 <= 3; b1 = b1 == -1 ? 1 : b1+1)
+        for (int c1 = -3; c1 <= 3; c1 = c1 == -1 ? 1 : c1+1)
+            for (int a2 = -5; a2 <= 5; a2 = a2 == -3 ? 3 : a2+1)
+            for (int b2 = -5; b2 <= 5; b2 = b2 == -3 ? 3 : b2+1)
+            for (int c2 = -5; c2 <= 5; c2 = c2 == -3 ? 3 : c2+1)
+                for (int a3 = -7; a3 <= 7;  a3 = a3 == -5 ? 5 : a3+1)
+                for (int b3 = -7; b3 <= 7;  b3 = b3 == -5 ? 5 : b3+1)
+                for (int c3 = -7; c3 <= 7;  c3 = c3 == -5 ? 5 : c3+1)
+                {
+                    if (Math.abs(a1) == Math.abs(b1) || Math.abs(a1) == Math.abs(c1) || Math.abs(b1) == Math.abs(c1)) continue;
+                    if (Math.abs(a2) == Math.abs(b2) || Math.abs(a2) == Math.abs(c2) || Math.abs(b2) == Math.abs(c2)) continue;
+                    if (Math.abs(a3) == Math.abs(b3) || Math.abs(a3) == Math.abs(c3) || Math.abs(b3) == Math.abs(c3)) continue;
+                    
+                    Join2BetweenTiers method = new Join2BetweenTiers();
+                    ITabularFormula formula = createFormula(a1, b1, c1, a3, b3, c3);
+                    
+                    ITier tier0 = formula.getTiers().get(0);
+                    ITier tier1 = formula.getTiers().get(1);
+                    
+                    SimpleTier tier = new SimpleTier(Math.abs(a2), Math.abs(b2), Math.abs(c2));
+                    tier.add(new SimpleTriplet(a2, b2, c2));
+                    try
+                    {
+                        assertTrue("Should join", method.tryJoin(formula, tier));
+                        assertTrue("Debugger", true);
+                        
+                        assertValueEquals(tier0, a1);
+                        assertValueEquals(tier0, b1);
+                        assertValueEquals(tier0, c1);
+                        
+                        assertValueEquals(tier, a2);
+                        assertValueEquals(tier, b2);
+                        assertValueEquals(tier, c2);
+                        
+                        assertValueEquals(tier1, a3);
+                        assertValueEquals(tier1, b3);
+                        assertValueEquals(tier1, c3);
+                    }
+                    catch (Throwable t)
+                    {
+                        System.out.println("Formula: " + formula);
+                        System.out.println("Tier: " + tier);
+                        
+                        throw t;
+                    }
+                }
+    }
+
+    private void assertValueEquals(ITier tier, int varName)
+    {
+        if (tier.getAName() == Math.abs(varName))
+        {
+            if (varName < 0) assertEquals(Value.AllNegative, tier.valueOfA()); else
+                        assertEquals(Value.AllPlain, tier.valueOfA());
+        }
+        else if (tier.getBName() == Math.abs(varName))
+        {
+            if (varName < 0) assertEquals(Value.AllNegative, tier.valueOfB()); else
+                        assertEquals(Value.AllPlain, tier.valueOfB());
+        }
+        else if (tier.getCName() == Math.abs(varName))
+        {
+            if (varName < 0) assertEquals(Value.AllNegative, tier.valueOfC()); else
+                        assertEquals(Value.AllPlain, tier.valueOfC());
+        }
     }
     
     @Test
@@ -221,6 +292,57 @@ public class TestJoinMethods
     }
 
     @Test
+    public void tryJoin3BetweenTiers_2Left1Right_BitsOnTheirPlaces() throws Throwable
+    {
+        for (int a1 = -3; a1 <= 3; a1 = a1 == -1 ? 1 : a1+1)
+        for (int b1 = -3; b1 <= 3; b1 = b1 == -1 ? 1 : b1+1)
+        for (int c1 = -3; c1 <= 3; c1 = c1 == -1 ? 1 : c1+1)
+            for (int a2 = -3; a2 <= 3; a2 = a2 == -1 ? 1 : a2+1)
+            for (int b2 = -3; b2 <= 3; b2 = b2 == -1 ? 1 : b2+1)
+            for (int c2 = -6; c2 <= 6; c2 = c2 == -4 ? 4 : c2+1)
+                for (int a3 = -6; a3 <= 6; a3 = a3 == -4 ? 4 : a3+1)
+                for (int b3 = -6; b3 <= 6; b3 = b3 == -4 ? 4 : b3+1)
+                for (int c3 = -6; c3 <= 6; c3 = c3 == -4 ? 4 : c3+1)
+                {
+                    if (Math.abs(a1) == Math.abs(b1) || Math.abs(a1) == Math.abs(c1) || Math.abs(b1) == Math.abs(c1)) continue;
+                    if (Math.abs(a2) == Math.abs(b2) || Math.abs(a2) == Math.abs(c2) || Math.abs(b2) == Math.abs(c2)) continue;
+                    if (Math.abs(a3) == Math.abs(b3) || Math.abs(a3) == Math.abs(c3) || Math.abs(b3) == Math.abs(c3)) continue;
+                    
+                    Join3BetweenTiers method = new Join3BetweenTiers();
+                    ITabularFormula formula = createFormula(a1, b1, c1, a3, b3, c3);
+                    ITier tier0 = formula.getTiers().get(0);
+                    ITier tier1 = formula.getTiers().get(1);
+                    
+                    SimpleTier tier = new SimpleTier(Math.abs(a2), Math.abs(b2), Math.abs(c2));
+                    tier.add(new SimpleTriplet(a2, b2, c2));
+                    try
+                    {
+                        assertTrue("Should join", method.tryJoin(formula, tier));
+                        assertTrue("Debugger", true);
+                        
+                        assertValueEquals(tier0, a1);
+                        assertValueEquals(tier0, b1);
+                        assertValueEquals(tier0, c1);
+                        
+                        assertValueEquals(tier, a2);
+                        assertValueEquals(tier, b2);
+                        assertValueEquals(tier, c2);
+                        
+                        assertValueEquals(tier1, a3);
+                        assertValueEquals(tier1, b3);
+                        assertValueEquals(tier1, c3);
+                    }
+                    catch (Throwable t)
+                    {
+                        System.out.println("Formula: " + formula);
+                        System.out.println("Tier: " + tier);
+                        
+                        throw t;
+                    }
+                }
+    }
+
+    @Test
     public void testJoin3BetweenTiers_2Left1Right_ThreeTiers() throws EmptyStructureException
     {
         Join3BetweenTiers method = new Join3BetweenTiers();
@@ -313,6 +435,57 @@ public class TestJoinMethods
                         formula.complete(createPermutation(1, 2, 3, 4, 5, 6));
                         
                         assertTrue(((ICompactTripletsStructure)formula).tiersSorted());
+                    }
+                    catch (AssertionError e)
+                    {
+                        System.out.println("Formula: " + formula);
+                        System.out.println("Tier: " + tier);
+                        
+                        throw e;
+                    }
+                }
+    }
+
+    @Test
+    public void tryJoin3BetweenTiers_1Left2Right_BitsOnTheirPlaces() throws EmptyStructureException
+    {
+        for (int a1 = -3; a1 <= 3; a1 = a1 == -1 ? 1 : a1+1)
+        for (int b1 = -3; b1 <= 3; b1 = b1 == -1 ? 1 : b1+1)
+        for (int c1 = -3; c1 <= 3; c1 = c1 == -1 ? 1 : c1+1)
+            for (int a2 = -3; a2 <= 3; a2 = a2 == -1 ? 1 : a2+1)
+            for (int b2 = -6; b2 <= 6; b2 = b2 == -4 ? 4 : b2+1)
+            for (int c2 = -6; c2 <= 6; c2 = c2 == -4 ? 4 : c2+1)
+                for (int a3 = -6; a3 <= 6; a3 = a3 == -4 ? 4 : a3+1)
+                for (int b3 = -6; b3 <= 6; b3 = b3 == -4 ? 4 : b3+1)
+                for (int c3 = -6; c3 <= 6; c3 = c3 == -4 ? 4 : c3+1)
+                {
+                    if (Math.abs(a1) == Math.abs(b1) || Math.abs(a1) == Math.abs(c1) || Math.abs(b1) == Math.abs(c1)) continue;
+                    if (Math.abs(a2) == Math.abs(b2) || Math.abs(a2) == Math.abs(c2) || Math.abs(b2) == Math.abs(c2)) continue;
+                    if (Math.abs(a3) == Math.abs(b3) || Math.abs(a3) == Math.abs(c3) || Math.abs(b3) == Math.abs(c3)) continue;
+                    
+                    Join3BetweenTiers method = new Join3BetweenTiers();
+                    ITabularFormula formula = createFormula(a1, b1, c1, a3, b3, c3);
+                    ITier tier0 = formula.getTiers().get(0);
+                    ITier tier1 = formula.getTiers().get(1);
+                    
+                    SimpleTier tier = new SimpleTier(Math.abs(a2), Math.abs(b2), Math.abs(c2));
+                    tier.add(new SimpleTriplet(a2, b2, c2));
+                    try
+                    {
+                        assertTrue("Should join", method.tryJoin(formula, tier));
+                        assertTrue("Debugger", true);
+                        
+                        assertValueEquals(tier0, a1);
+                        assertValueEquals(tier0, b1);
+                        assertValueEquals(tier0, c1);
+                        
+                        assertValueEquals(tier, a2);
+                        assertValueEquals(tier, b2);
+                        assertValueEquals(tier, c2);
+                        
+                        assertValueEquals(tier1, a3);
+                        assertValueEquals(tier1, b3);
+                        assertValueEquals(tier1, c3);
                     }
                     catch (AssertionError e)
                     {
@@ -444,6 +617,48 @@ public class TestJoinMethods
     }
 
     @Test
+    public void tryJoin1Left_BitsOnTheirPlaces() throws EmptyStructureException
+    {
+        for (int a1 = -3; a1 <= 3; a1 = a1 == -1 ? 1 : a1+1)
+        for (int b1 = -3; b1 <= 3; b1 = b1 == -1 ? 1 : b1+1)
+        for (int c1 = -3; c1 <= 3; c1 = c1 == -1 ? 1 : c1+1)
+            for (int a2 = -5; a2 <= 5; a2 = a2 == -3 ? 3 : a2+1)
+            for (int b2 = -5; b2 <= 5; b2 = b2 == -3 ? 3 : b2+1)
+            for (int c2 = -5; c2 <= 5; c2 = c2 == -3 ? 3 : c2+1)
+            {
+                if (Math.abs(a1) == Math.abs(b1) || Math.abs(a1) == Math.abs(c1) || Math.abs(b1) == Math.abs(c1)) continue;
+                if (Math.abs(a2) == Math.abs(b2) || Math.abs(a2) == Math.abs(c2) || Math.abs(b2) == Math.abs(c2)) continue;
+                
+                Join1Left method = new Join1Left();
+                ITabularFormula formula = createFormula(a2, b2, c2);
+                ITier tier0 = formula.getTiers().get(0);
+                
+                SimpleTier tier = new SimpleTier(Math.abs(a1), Math.abs(b1), Math.abs(c1));
+                tier.add(new SimpleTriplet(a1, b1, c1));
+                try
+                {
+                    assertTrue("Should join", method.tryJoin(formula, tier));
+                    assertTrue("Debugger", true);
+                    
+                    assertValueEquals(tier0, a2);
+                    assertValueEquals(tier0, b2);
+                    assertValueEquals(tier0, c2);
+                    
+                    assertValueEquals(tier, a1);
+                    assertValueEquals(tier, b1);
+                    assertValueEquals(tier, c1);
+                }
+                catch (AssertionError e)
+                {
+                    System.out.println("Formula: " + formula);
+                    System.out.println("Tier: " + tier);
+                    
+                    throw e;
+                }
+            }
+    }
+
+    @Test
     public void tryJoin1Right() throws EmptyStructureException
     {
         for (int a1 = 1; a1 <= 3; a1++)
@@ -480,7 +695,48 @@ public class TestJoinMethods
                 }
             }
     }
-    
+
+    @Test
+    public void tryJoin1Right_BitsOnTheirPlaces() throws EmptyStructureException
+    {
+        for (int a1 = -3; a1 <= 3; a1 = a1 == -1 ? 1 : a1+1)
+        for (int b1 = -3; b1 <= 3; b1 = b1 == -1 ? 1 : b1+1)
+        for (int c1 = -3; c1 <= 3; c1 = c1 == -1 ? 1 : c1+1)
+            for (int a2 = -5; a2 <= 5; a2 = a2 == -3 ? 3 : a2+1)
+            for (int b2 = -5; b2 <= 5; b2 = b2 == -3 ? 3 : b2+1)
+            for (int c2 = -5; c2 <= 5; c2 = c2 == -3 ? 3 : c2+1)
+            {
+                if (Math.abs(a1) == Math.abs(b1) || Math.abs(a1) == Math.abs(c1) || Math.abs(b1) == Math.abs(c1)) continue;
+                if (Math.abs(a2) == Math.abs(b2) || Math.abs(a2) == Math.abs(c2) || Math.abs(b2) == Math.abs(c2)) continue;
+                
+                Join1Right method = new Join1Right();
+                ITabularFormula formula = createFormula(a2, b2, c2);
+                ITier tier0 = formula.getTiers().get(0);
+                
+                SimpleTier tier = new SimpleTier(Math.abs(a1), Math.abs(b1), Math.abs(c1));
+                tier.add(new SimpleTriplet(a1, b1, c1));
+                try
+                {
+                    assertTrue("Should join", method.tryJoin(formula, tier));
+                    assertTrue("Debugger", true);
+                    
+                    assertValueEquals(tier0, a2);
+                    assertValueEquals(tier0, b2);
+                    assertValueEquals(tier0, c2);
+                    
+                    assertValueEquals(tier, a1);
+                    assertValueEquals(tier, b1);
+                    assertValueEquals(tier, c1);
+                }
+                catch (AssertionError e)
+                {
+                    System.out.println("Formula: " + formula);
+                    System.out.println("Tier: " + tier);
+                    
+                    throw e;
+                }
+            }
+    }
 
     @Test
     public void tryJoin2Left() throws EmptyStructureException
@@ -521,6 +777,48 @@ public class TestJoinMethods
     }
 
     @Test
+    public void tryJoin2Left_BitsOnTheirPlaces() throws EmptyStructureException
+    {
+        for (int a1 = -3; a1 <= 3; a1 = a1 == -1 ? 1 : a1+1)
+        for (int b1 = -3; b1 <= 3; b1 = b1 == -1 ? 1 : b1+1)
+        for (int c1 = -3; c1 <= 3; c1 = c1 == -1 ? 1 : c1+1)
+            for (int a2 = -4; a2 <= 4; a2 = a2 == -2 ? 2 : a2+1)
+            for (int b2 = -4; b2 <= 4; b2 = b2 == -2 ? 2 : b2+1)
+            for (int c2 = -4; c2 <= 4; c2 = c2 == -2 ? 2 : c2+1)
+            {
+                if (Math.abs(a1) == Math.abs(b1) || Math.abs(a1) == Math.abs(c1) || Math.abs(b1) == Math.abs(c1)) continue;
+                if (Math.abs(a2) == Math.abs(b2) || Math.abs(a2) == Math.abs(c2) || Math.abs(b2) == Math.abs(c2)) continue;
+                
+                Join2Left method = new Join2Left();
+                ITabularFormula formula = createFormula(a2, b2, c2);
+                ITier tier0 = formula.getTiers().get(0);
+                
+                SimpleTier tier = new SimpleTier(Math.abs(a1), Math.abs(b1), Math.abs(c1));
+                tier.add(new SimpleTriplet(a1, b1, c1));
+                try
+                {
+                    assertTrue("Should join", method.tryJoin(formula, tier));
+                    assertTrue("Debugger", true);
+                    
+                    assertValueEquals(tier0, a2);
+                    assertValueEquals(tier0, b2);
+                    assertValueEquals(tier0, c2);
+                    
+                    assertValueEquals(tier, a1);
+                    assertValueEquals(tier, b1);
+                    assertValueEquals(tier, c1);
+                }
+                catch (AssertionError e)
+                {
+                    System.out.println("Formula: " + formula);
+                    System.out.println("Tier: " + tier);
+                    
+                    throw e;
+                }
+            }
+    }
+    
+    @Test
     public void tryJoin2Right() throws EmptyStructureException
     {
         for (int a1 = 1; a1 <= 3; a1++)
@@ -558,5 +856,46 @@ public class TestJoinMethods
             }
     }
 
+    @Test
+    public void tryJoin2Right_BitsOnTheirPlaces() throws EmptyStructureException
+    {
+        for (int a1 = -3; a1 <= 3; a1 = a1 == -1 ? 1 : a1+1)
+        for (int b1 = -3; b1 <= 3; b1 = b1 == -1 ? 1 : b1+1)
+        for (int c1 = -3; c1 <= 3; c1 = c1 == -1 ? 1 : c1+1)
+            for (int a2 = -4; a2 <= 4; a2 = a2 == -2 ? 2 : a2+1)
+            for (int b2 = -4; b2 <= 4; b2 = b2 == -2 ? 2 : b2+1)
+            for (int c2 = -4; c2 <= 4; c2 = c2 == -2 ? 2 : c2+1)
+            {
+                if (Math.abs(a1) == Math.abs(b1) || Math.abs(a1) == Math.abs(c1) || Math.abs(b1) == Math.abs(c1)) continue;
+                if (Math.abs(a2) == Math.abs(b2) || Math.abs(a2) == Math.abs(c2) || Math.abs(b2) == Math.abs(c2)) continue;
+                
+                Join2Right method = new Join2Right();
+                ITabularFormula formula = createFormula(a2, b2, c2);
+                ITier tier0 = formula.getTiers().get(0);
+                
+                SimpleTier tier = new SimpleTier(Math.abs(a1), Math.abs(b1), Math.abs(c1));
+                tier.add(new SimpleTriplet(a1, b1, c1));
+                try
+                {
+                    assertTrue("Should join", method.tryJoin(formula, tier));
+                    assertTrue("Debugger", true);
+                    
+                    assertValueEquals(tier0, a2);
+                    assertValueEquals(tier0, b2);
+                    assertValueEquals(tier0, c2);
+                    
+                    assertValueEquals(tier, a1);
+                    assertValueEquals(tier, b1);
+                    assertValueEquals(tier, c1);
+                }
+                catch (AssertionError e)
+                {
+                    System.out.println("Formula: " + formula);
+                    System.out.println("Tier: " + tier);
+                    
+                    throw e;
+                }
+            }
+    }
 }
 
